123112d39442944a3aa0aafd9d017eb6643e3d6f
davidhei2023 <davidhei2010@gmail.com>
2024-06-14 16:47:47 +0300

    yolo5 v0.0.1

--- yolo5/AMI-commands.sh
+++ yolo5/AMI-commands.sh
@@ -21,4 +21,4 @@ systemctl start docker
 groupadd docker
 usermod -aG docker $USER
 
-docker run -d --name polybot -p 8443:8443 -e aws_region=us-east-2 -e TELEGRAM_APP_URL=https://davidhei-polybot.int-devops.click -e POLYBOT_RESULTS_URL=https://davidhei-polybot.int-devops.click -e DYNAMODB_TABLE_NAME=davidhei-AWS-polybot-DynamoDB-table davidhei/aws-polybotservice:polybot-v0.2.0
+docker run -d --name yolo5 -e aws_region=us-east-2 davidhei/aws-polybotservice:yolo5-v0.0.1
--- yolo5/app.py
+++ yolo5/app.py
@@ -8,17 +8,36 @@ import boto3
 import requests
 import json
 
-images_bucket = os.environ['BUCKET_NAME']
-queue_url = os.environ['SQS_QUEUE_URL']
-dynamodb_table_name = os.environ['DYNAMODB_TABLE_NAME']
-polybot_results_url = os.environ['POLYBOT_RESULTS_URL']
+images_bucket = os.environ.get('BUCKET_NAME', 'default-bucket')
+queue_url = os.environ.get('SQS_QUEUE_URL', 'default-sqs-url')
+dynamodb_table_name = os.environ.get('DYNAMODB_TABLE_NAME', 'default-dynamodb-table')
+polybot_results_url = os.environ.get('POLYBOT_RESULTS_URL', 'default-results-url')
 
 sqs_client = boto3.client('sqs', region_name='us-east-2')
 s3_client = boto3.client('s3', region_name='us-east-2')
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table(dynamodb_table_name)
 
-with open("data/coco128.yaml", "r") as stream:
+coco_yaml_path = "data/coco128.yaml"
+if not os.path.exists(coco_yaml_path):
+    logger.warning(f"File '{coco_yaml_path}' not found. Creating default coco128.yaml...")
+
+    # Create a default coco128.yaml file
+    default_yaml = {
+        'names': [
+            'person',
+            'bicycle',
+            'car',
+            # Add more classes as needed
+        ]
+    }
+
+    with open(coco_yaml_path, 'w') as yaml_file:
+        yaml.dump(default_yaml, yaml_file, default_flow_style=False)
+
+    logger.info(f"Created default coco128.yaml at '{coco_yaml_path}'")
+
+with open(coco_yaml_path, "r") as stream:
     names = yaml.safe_load(stream)['names']
 
 
@@ -33,19 +52,16 @@ def consume():
 
             logger.info(f'prediction: {prediction_id}. start processing')
 
-            # Extract image name and chat ID from the message
             img_name = message['img_name']
             chat_id = message['chat_id']
             original_img_path = f'/tmp/{img_name}'
 
-            # Download the image from S3
             s3_client.download_file(images_bucket, img_name, original_img_path)
             logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
-            # Perform object detection using YOLOv5
             run(
                 weights='yolov5s.pt',
-                data='data/coco128.yaml',
+                data=coco_yaml_path,
                 source=original_img_path,
                 project='static/data',
                 name=prediction_id,
@@ -53,14 +69,11 @@ def consume():
             )
             logger.info(f'prediction: {prediction_id}/{original_img_path}. done')
 
-            # Predicted image path
             predicted_img_path = Path(f'static/data/{prediction_id}/{img_name}')
 
-            # Upload the predicted image to S3
             s3_client.upload_file(str(predicted_img_path), images_bucket, f'predicted/{img_name}')
             logger.info(f'prediction: {prediction_id}. Uploaded predicted image to S3')
 
-            # Parse prediction labels and create a summary
             pred_summary_path = Path(f'static/data/{prediction_id}/labels/{img_name.split(".")[0]}.txt')
             if pred_summary_path.exists():
                 with open(pred_summary_path) as f:
@@ -84,18 +97,15 @@ def consume():
                     'time': time.time()
                 }
 
-                # Store the prediction summary in DynamoDB
                 table.put_item(Item=prediction_summary)
                 logger.info(f'prediction: {prediction_id}. Stored prediction summary in DynamoDB')
 
-                # Notify Polybot microservice
                 response = requests.post(f'{polybot_results_url}/results', params={'predictionId': prediction_id})
                 if response.status_code == 200:
                     logger.info(f'prediction: {prediction_id}. Notified Polybot microservice successfully')
                 else:
                     logger.error(f'prediction: {prediction_id}. Failed to notify Polybot microservice')
 
-            # Delete the message from the queue as the job is considered as DONE
             sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
             logger.info(f'prediction: {prediction_id}. Deleted message from SQS queue')
 
