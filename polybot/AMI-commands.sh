dd5352e41be8cbce2b8cc7cbdf961e5953b25380
davidhei2023 <davidhei2010@gmail.com>
2024-06-28 00:18:37 +0300

    GitHub actions polybot test

--- polybot/AMI-commands.sh
+++ polybot/AMI-commands.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
 apt-get update
 apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
@@ -18,8 +16,11 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docke
 
 systemctl enable docker
 systemctl start docker
-
 groupadd docker
 usermod -aG docker $USER
 
-docker run -d --name polybot --restart always -p 8443:8443 davidhei/aws-polybotservice:polybot-v1.3.11
+docker pull davidhei/polybot:latest
+docker stop polybot || true
+docker rm polybot || true
+
+docker run -d --name polybot --restart always -p 8443:8443 davidhei/polybot:latest
